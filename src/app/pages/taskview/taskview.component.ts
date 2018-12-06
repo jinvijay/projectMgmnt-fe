480db8d154ad59f8352f201be22713bcea8ce8b6
Jinu Vijay <jinu.vijay@gmail.com>
2018-12-05 23:41:01 -0500

    View tasks

--- src/app/pages/taskview/taskview.component.ts
+++ src/app/pages/taskview/taskview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../modal/task';
+import { ActivatedRoute } from '@angular/router'
+import { ProjManagementService } from '../../service/proj-management.service';
+
 
 @Component({
   selector: 'app-taskview',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskviewComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[];
+  serviceMessage: string;
+  errorMsg: string;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private pmService: ProjManagementService) { }
 
   ngOnInit() {
+    this.refreshTasks();
+  }
+
+  refreshTasks() {
+    this.pmService.listTasks().subscribe(
+      data => { this.tasks = data; this.errorMsg = null },
+      error => { this.errorMsg = "Error in calling server"; console.log(error); }
+    );
   }
 
 }
